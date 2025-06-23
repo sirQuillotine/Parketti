ff79f599a58430faf61b4fe938756445c550b84f
Santeri Kuusela <santeriviljokuusela@gmail.com>
2025-06-23 18:20:55 +0300

    Fixed bug in registering and login

--- app.py
+++ app.py
@@ -50,9 +50,37 @@ def index(page=1):
     return render_template("index.html", query=query, events=events, page=page,
                             page_count=page_count, session=session)
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+            username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        flash("VIRHE: salasanat eivät ole samat")
+        return render_template("register.html", filled=username)
+    password_hash = generate_password_hash(password1)
+
+    if not username.isalpha():
+        flash("VIRHE: käyttäjätunnus saa sisältää vain kirjaimia")
+        return render_template("register.html", filled=username)
+    if len(username) < 3 or len(username) > 20:
+        flash("VIRHE: käyttäjätunnuksen tulee olla 3-20 merkkiä pitkä")
+        return render_template("register.html", filled=username)
+    if len(password1) < 3:
+        flash("VIRHE: salasanan tulee olla vähintään 3 merkkiä pitkä")
+        return render_template("register.html", filled=username)
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        flash("VIRHE: tunnus on jo varattu")
+        return render_template("register.html", filled=username)
+    flash("Käyttäjä luotu onnistuneesti")
+    return redirect("/")
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
@@ -204,9 +232,10 @@ def login():
         password = request.form["password"]
 
         sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        password_hash = db.query(sql, [username])[0][0] if db.query(sql, [username]) else ""
         if not check_password_hash(password_hash, password):
-            return "VIRHE: väärä tunnus tai salasana"
+            flash("VIRHE: väärä tunnus tai salasana") 
+            return redirect("/login")
         session["username"] = username
         session["csrf_token"] = secrets.token_hex(16)
         return redirect("/")
@@ -215,35 +244,6 @@ def login():
 def logout():
     del session["username"]
     return redirect("/")
-
-@app.route("/create", methods=["POST"])
-def create():
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
-    if password1 != password2:
-        flash("VIRHE: salasanat eivät ole samat")
-        return redirect("/register")
-    password_hash = generate_password_hash(password1)
-
-    if not username.isalpha():
-        flash("VIRHE: käyttäjätunnus saa sisältää vain kirjaimia")
-        return redirect("/register")
-    if len(username) < 3 or len(username) > 20:
-        flash("VIRHE: käyttäjätunnuksen tulee olla 3-20 merkkiä pitkä")
-        return redirect("/register")
-    if len(password1) < 3:
-        flash("VIRHE: salasanan tulee olla vähintään 3 merkkiä pitkä")
-        return redirect("/register")
-    try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-    except sqlite3.IntegrityError:
-        flash("VIRHE: tunnus on jo varattu")
-        return redirect("/register")
-    flash("Käyttäjä luotu onnistuneesti")
-    return redirect("/register")
-
 
 def check_csrf():
     if "csrf_token" not in request.form:
